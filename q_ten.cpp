3442fced08d7bce3a056da63753fc67e871fe42c
hanbing <beatmight@gmail.com>
2019-08-30 13:54:33 +0800

    dp solution

--- q_ten.cpp
+++ q_ten.cpp
@@ -6,18 +6,15 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         bool dp[p.size()+1][s.size()+1] = {false};
-
-        for(int i=0; i<=p.size();){
-            bool wild_card = i<p.size() and p[i+1] == '*'? true: false;
-
-            for (int j=0; j<s.size();){
-                dp[i][j] == true;
-                if(wild_card){
-                    dp[i]
-                }
-            }
+        dp[0][0] = true;
+        for (int j = 1; j < s.size() + 1; j++)
+            dp[0][j] = s.size() == 0;
+        for (int i = 1; i < p.size() + 1; i++) {
+            bool ast = p[i-1] == '*';
+            dp[i][0] = ast && dp[i-2][0];
+            for (int j = 1; j < s.size() + 1; j++)
+                dp[i][j] = ast && dp[i-2][j] || (p[i-1-ast] == '.' || p[i-1-ast] == s[j-1]) && dp[i-1+ast][j-1];
         }
-
         return dp[p.size()][s.size()];
     }
 };
